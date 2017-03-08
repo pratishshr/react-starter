fef90a9cdc053539e057bfa184346d50dce07de2
pratishshr <vanroshr@gmail.com>
2017-03-08 13:20:11 +0545

    Revert "Remove /"
    This reverts commit a745e3d806b8aa515ef6e9b7cde928461d44557d.

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -25,11 +25,11 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        use: 'file-loader?name=[name].[ext]&publicPath=react-starter/images/&outputPath=/images/'
+        use: 'file-loader?name=[name].[ext]&publicPath=images/&outputPath=/images/'
       },
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: 'file-loader?name=[name].[ext]&publicPath=react-starter/fonts/&outputPath=/fonts/'
+        use: 'file-loader?name=[name].[ext]&publicPath=fonts/&outputPath=/fonts/'
       },
       {
         test: /\.scss$/,
