0fc04a257bc955a4de1fe884127a8eeb68ea75d7
Saugat Acharya <mesaugat@gmail.com>
2017-03-29 23:34:26 +0545

    Use react-router v4

--- src/components/commons/PageNotFound.js
+++ src/components/commons/PageNotFound.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import reactLogo from '../../../public/images/react-logo.png';
+
 class PageNotFound extends Component {
   render() {
     return (
       <div className="not-found-container">
+        <div className="logo-container">
+          <img src={reactLogo} alt="react-logo"/>
+        </div>
         <div className="not-found-status">404</div>
         <div className="not-found-message">Page Not Found</div>
       </div>
