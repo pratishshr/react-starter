06141071c4862162beb291b8fdeb9b4e6d2f99a7
pratishshr <vanroshr@gmail.com>
2017-04-06 00:44:43 +0545

    Add routes for documentations

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,20 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import routeConstants from './constants/routeConstants';
 
 import Home from './components/home';
+import Guides from './components/guides';
+import Navbar from './components/commons/Navbar';
 import PageNotFound from './components/commons/PageNotFound';
 
 const baseHref = process.env.BASE_HREF || '/';
 
 const Router = () => (
   <BrowserRouter basename={baseHref}>
-    <div className="container">
+    <div className="container-fluid main-container">
+      <Navbar />
       <Switch>
-        <Route exact path={routeConstants.HOME} component={Home} />
-        <Route component={PageNotFound} />
+        <Route exact path={routeConstants.HOME} component={Home}/>
+        <Route path={routeConstants.DOCS} component={Guides}/>
+        <Route component={PageNotFound}/>
       </Switch>
     </div>
   </BrowserRouter>
